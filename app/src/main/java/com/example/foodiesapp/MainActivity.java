9208f9e6f0cbbba26d6cfe3f30d2511088a053ee
seregacarbon <ekvaserega@gmail.com>
2020-05-07 00:17:09 +0300

    designed-toolbar-and-added-filter-button

--- app/src/main/java/com/example/foodiesapp/MainActivity.java
+++ app/src/main/java/com/example/foodiesapp/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.foodiesapp;
 
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +10,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.foodiesapp.utils.AppBarTuner;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,40 +19,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
 
-        ActionBar toolbar = getSupportActionBar();
-        if(toolbar != null) {
-            toolbar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-            toolbar.setCustomView(R.layout.app_bar);
-        }
-    }
+        ActionBar toolbar = AppBarTuner.tunedToolBar(getSupportActionBar(), R.layout.app_bar);
+        SearchView searchView =
+                AppBarTuner.tunedSearchView(toolbar, findViewById(R.id.search_view), findViewById(R.id.left_menu_icon));
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-        MenuItem item = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) item.getActionView();
-        searchView.setQueryHint(getResources().getString(R.string.query_hint));
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-
-
                 return false;
             }
         });
-        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            SearchView sv = findViewById(R.id.search_view);
+            if(!sv.isIconified()) {
+                sv.setQuery("", false);
+                sv.setIconified(true);
+            }
+            else {
+                //TODO
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/foodiesapp/utils/AppBarTuner.java
@@ -0,0 +1,40 @@
+package com.example.foodiesapp.utils;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
+
+public final class AppBarTuner {
+
+    public static ActionBar tunedToolBar(ActionBar toolbar, int customViewResourceId) {
+        if(toolbar != null) {
+            toolbar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            toolbar.setCustomView(customViewResourceId);
+            Toolbar parent = (Toolbar)toolbar.getCustomView().getParent();
+            parent.setPadding(0,0,0,0);
+            parent.setContentInsetsAbsolute(0,0);
+        }
+        return toolbar;
+    }
+
+    public static SearchView tunedSearchView(ActionBar toolBar, SearchView searchView, ImageView leftMenuImageView) {
+
+        searchView.setOnSearchClickListener(v -> {
+            leftMenuImageView.setVisibility(View.INVISIBLE);
+            Objects.requireNonNull(toolBar).setDisplayHomeAsUpEnabled(true);
+        });
+
+        searchView.setOnCloseListener(() -> {
+            Objects.requireNonNull(toolBar).setDisplayHomeAsUpEnabled(false);
+            leftMenuImageView.setVisibility(View.VISIBLE);
+            return false;
+        });
+
+        return searchView;
+    }
+}
