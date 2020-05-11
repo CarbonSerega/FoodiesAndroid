d3dede28825a7d9aca362a2d412b7f5fd155c313
seregacarbon <ekvaserega@gmail.com>
2020-05-11 23:07:05 +0300

    added-network-connectivity-manager

--- app/src/main/java/com/example/foodiesapp/utils/web/Connectivity.java
+++ app/src/main/java/com/example/foodiesapp/utils/web/Connectivity.java
@@ -1,18 +1,77 @@
 package com.example.foodiesapp.utils.web;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 
-public class Connectivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static boolean checkInternetConnection(Context context) {
-        ConnectivityManager connectivityManager =
-                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if(connectivityManager != null) {
-            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
-            return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+public class Connectivity extends BroadcastReceiver {
+
+    protected List<NetworkStateReceiverListener> listeners;
+    protected Boolean connected;
+    private final String TAG = "NetworkStateReceiver";
+
+    public Connectivity() {
+        listeners = new ArrayList<>();
+        connected = null;
+    }
+
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Intent broadcast received");
+        if(intent == null || intent.getExtras() == null)
+            return;
+
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+         NetworkInfo networkInfo = null;
+        if (manager != null) {
+            networkInfo = manager.getActiveNetworkInfo();
         }
-        return false;
+
+        if(networkInfo != null && networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+            connected = true;
+        } else if(intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, Boolean.FALSE)) { 
+            connected = false;
+        }
+
+        notifyStateToAll();
+    }
+
+    private void notifyStateToAll() {
+        Log.i(TAG, "Notifying state to " + listeners.size() + " listener(s)");
+        for(NetworkStateReceiverListener eachNetworkStateReceiverListener : listeners)
+            notifyState(eachNetworkStateReceiverListener);
+    }
+
+
+    private void notifyState(NetworkStateReceiverListener networkStateReceiverListener) {
+        if(connected == null || networkStateReceiverListener == null)
+            return;
+
+        if(connected) {
+            networkStateReceiverListener.networkAvailable();
+        } else {
+            networkStateReceiverListener.networkUnavailable();
+        }
+    }
+
+    public void addListener(NetworkStateReceiverListener networkStateReceiverListener) {
+        Log.i(TAG, "addListener() - listeners.add(networkStateReceiverListener) + notifyState(networkStateReceiverListener);");
+        listeners.add(networkStateReceiverListener);
+        notifyState(networkStateReceiverListener);
+    }
+
+    public void removeListener(NetworkStateReceiverListener networkStateReceiverListener) {
+        listeners.remove(networkStateReceiverListener);
+    }
+
+    public interface NetworkStateReceiverListener {
+        void networkAvailable();
+        void networkUnavailable();
     }
 }
